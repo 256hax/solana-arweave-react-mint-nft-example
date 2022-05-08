bc9f5f479f6d6b943f9d72c83411375097cd89c6
256hax <256hax@git>
2022-05-09 00:01:26 +0900

    style: minor change

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,10 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 export const Header = () => {
   return(
-    <AppBar position="static" style={{ backgroundColor: "#000000" }}>
+    <AppBar position="static" style={{ backgroundColor: "#000000" }} sx={{ mb: 3 }}>
       <Toolbar>
-        Mint NFT
+        <Typography>Mint NFT (Arweave and Solana Chain)</Typography>
       </Toolbar>
     </AppBar>
   );
